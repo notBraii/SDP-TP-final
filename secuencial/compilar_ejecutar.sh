bc0edbcc1f4b6b473544af742502e8a5b031d0ed
nicopache97 <nicopacheco1997@gmail.com>
2024-05-23 15:56:14 -0300

    Scrypt para compilar y ejecutar por lotes

---
+++ secuencial/compilar_ejecutar.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# compilar proyecto
+gcc -pthread -o secuencial secuencial.c simple_init.c check.c ordenar_secuencial.c -lm 
+
+# Nombre del archivo ejecutable
+ejecutable="./secuencial"
+
+# Parámetros para cada ejecución
+parametros=(
+    "20 0"
+    "20 1"
+    "20 2"
+)
+
+# Nombre de la subcarpeta para los archivos .dat
+subcarpeta="resultados"
+
+# Crear la subcarpeta si no existe
+mkdir -p $subcarpeta
+
+# Número total de ejecuciones
+num_ejecuciones=${#parametros[@]}
+
+# Loop para ejecutar el programa secuencial con diferentes parámetros
+for ((i=0; i<num_ejecuciones; i++)); do
+    # Nombre del archivo de salida
+    archivo_salida="$subcarpeta/dat_$((i+1)).dat"
+
+    # Ejecutar el programa con los parámetros correspondientes y guardar la salida en la subcarpeta
+    echo "Ejecutando $ejecutable con parámetros ${parametros[i]} y guardando salida en $archivo_salida"
+    $ejecutable ${parametros[i]} > $archivo_salida
+done
